cced0094542fa62bbdab17bc4909c0f517071508
tenxteng <86tenx@gmail.com>
2024-10-30 10:21:02 +0100

    favicon

---
+++ src/components/Logo.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Logo = () => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="32" height="32" rx="8" fill="#4F46E5" />
+    <path
+      d="M9 16.5L14 21.5L23 10.5"
+      stroke="white"
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default Logo; 
